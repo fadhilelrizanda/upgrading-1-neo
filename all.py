064b54aadc4996f4415a1c3ae4865d32a0aed4f7
fadhilelrizanda <fadhilelrizanda@gmail.com>
2021-07-09 21:48:53 +0700

    first commit

---
+++ all.py
@@ -0,0 +1,79 @@
+# Comment
+
+# * Tipe Data
+# ini_string = "kalimat"
+# ini_int = 19
+# ini_float = 10.23
+
+# * Print Syntax
+# print(type(ini_string))
+# print(type(ini_int))
+
+# * Input
+# nama = input("Masukkan Nama : ")
+# print(nama)
+
+# * Condition
+# if ini_int == 10:
+#     print("Benar")
+# else:
+#     print("Salah")
+
+# * Loop
+# for n in range(10):
+#     print(n)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     print(x)
+
+# i = 7
+# while i > 1:
+#     print(i)
+#     i -= 1
+
+# # * Change data type
+# umur = 17
+# umur = str(umur)
+# print(umur)
+# print(type(umur))
+
+# * Ternary
+# x, y = 10, 20
+
+# coordinate = x if x < y else y
+# print(coordinate)
+
+# # * List
+# a = []
+# b = [1, 2, 3]
+# print(b[0])
+# c = list()
+# print(type(c))
+
+# # * Tuple
+# name = ("fadhil", "lukman", "andra")
+# print(name[1])
+
+# * Dictionary
+
+# daftar = {
+#     "nama": "lukman",
+#     "angkatan": 20,
+#     "ukm": "neo telemetri"
+# }
+# print(daftar)
+# print(daftar["nama"])
+
+# * Set
+# ini_set = {"sepatu", "sandal", "kaus kaki"}
+
+# for x in ini_set:
+#     print(x)
+
+# * String Template
+# name = "lukman"
+# angkatan = 20
+# # user = "Nama Saya  {0}, Angkatan {1}".format((name, angkatan))
+# # print(user)
+# print("Nama saya " + name + ", Angkatan " + str(angkatan))
